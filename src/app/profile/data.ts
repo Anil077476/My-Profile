39323ca140a119e385e7e48f5f3ef0e17741670a
Tilak Puli <tilakpuli15@gmail.com>
2023-08-12 20:45:50 +0530

    update resume and add two more tech to animation

--- src/app/profile/data.ts
+++ src/app/profile/data.ts
@@ -6,13 +6,13 @@ const convertToHtmlLink = (name: string, url: string) => `<a href="${url}">${nam
 const about: About = {
   heading: 'About me',
   mainIntroduction:
-    // prettier-ignore
+  // prettier-ignore
     'Hello! I\'m Tilak puli, an engineer at core who loves creating stuff that improves life. As a student, I was particularly fascinated by software development after seeing what a single line of code can do at scale (at that time I didn\'t know how much code runs behind it).',
   extendedIntroduction:
     'Fast-forward to career life,and I was fortunate to learn programming fundamentals, teamwork and methodologies like Agile, TDD with the help of great mentors in ' +
     convertToHtmlLink('STEP', 'https://www.thoughtworks.com/STEP') +
     ' Internship. During Internship, along with programming concepts mentors immensely focused on teamwork, soft skills and personal growth. After Internship, I have worked with multiple MNC clients in finance and transportation domain and helped them reach their goal with User-friendly and highly efficient Web applications.',
-  techStacks: ['Javascript', 'React', 'Node.js', 'React Native', 'Java', 'ROR', 'AWS'],
+  techStacks: ['Javascript', 'React', 'Node.js', 'React Native', 'Java', 'ROR', 'AWS']
 };
 
 const Gojek: Employment = {
@@ -20,29 +20,40 @@ const Gojek: Employment = {
   company: 'Gojek',
   range: '2019 May - 2019 Dec',
   notes: [
-    'Developed a system to collect employee feedback and reviews to calibrate ratings and hikes',
-    'Rewrote Node.js backend using Ruby on Rails and migrated from MongoDB to PostgresDB for a cleaner and more stable product',
-    'Contributed to all aspects ofthe productincluding creating user stories, building React UI, developing backend features, and running applications in Kubernetes',
-    'Traveled to Indonesia to discuss new requirements and provided user supportfor queries ',
-    'Improved the Testing coverage from 40% to 90%+ for existing and new backend and frontend repositories'
+    'Designed and Developed a React Native module to handle BLE communication between Android and IOS devices for an open-source identity management mobile application.',
+    'Focused on the Android BLE layer using Kotlin and achieved a significant improvement in transfer speed, reducing transfer time from 40 seconds to approximately 1 second for 150KB of data ',
+    'Contributed to the development of a React Native mobile app that enables identity management for 20+ millions of users across different countries and organizations',
+    'Trained and mentored junior developers, teaching skills in Clean Code and React Native to improve overall team performance.'
   ],
-  link: new URL('https://www.gojek.io/'),
+  link: new URL('https://www.gojek.io/')
 };
 
-const GS: Employment = {
+const GS1: Employment = {
   role: 'Software Engineer',
-  company: 'Goldman Sachs',
+  company: 'Goldman Sachs - Control Plane',
+  range: '2020 Mar - 2020 Dec',
+  notes: [
+    'Designed and Developed a web dashboard to manage 300+ AWS accounts in multiple regions with a maker-checker flow',
+    'Used AWS APIs, SSM documents, and Lambdas to perform customisable complex operations in AWS',
+    'Lead UX/UI design and development of React application from scratch using Redux, Jest, React testing library, Ant Design system and axios.',
+    'Worked on Spring Boot backend to invoke complex AWS APIs and respond back to UI with response that is paginated and structured',
+    'Helped new team members onboard to team and getting up to speed and handle maintenance.'
+  ],
+  link: new URL('https://www.goldmansachs.com/')
+};
+
+const GS2: Employment = {
+  role: 'Software Engineer',
+  company: 'Goldman Sachs - Analytics',
   range: '2020 Mar - 2022 May',
   notes: [
-    'Designed and Developed a dashboard to manage AWS resources with a maker-checker fl ow using AWS APIs, SSM documents, and Lambdas on multiple regions and 300+ accounts',
-    'Lead UX/UI design and development of React application from scratch',
-    'Actively collected feedback from users and team to improve usability',
-    'Worked on Spring Boot backend to invoke AWS APIs and helped new team members onboard to team and getting up to speed',
-    'Worked on Analytics dashboard which shows realtime visualization of millions oftransactions and accounts',
-    'Helped existing team to improve UX design and speed of analytics web application',
-    'Also worked with backend developers to improve API performance and better ways pre-compute and cache the data'
+    'Worked on existing Analytics dashboard which shows real time visualization of 300 thousands of transactions and 2000+ accounts',
+    'Helped existing team to improve UX design using design systems, pagination and restructuring pages using React, Redux, High Charts and Material UI.',
+    'Also worked on increasing speed of analytics API’s which perform real time computations on 300K+ transactions and accounts. Reduced the response time from over a minute ot less than 10 secs with help of pre-compute and cache the data',
+    'Worked on credit team’s Spring boot micro services which are crucial part in the system',
+    'Worked on User facing Web application to create screens for analytics in a Mono repo React application'
   ],
-  link: new URL('https://www.goldmansachs.com/'),
+  link: new URL('https://www.goldmansachs.com/')
 };
 
 const Mosip: Employment = {
@@ -50,24 +61,26 @@ const Mosip: Employment = {
   company: 'Mosip',
   range: '2022 Sep - Present',
   notes: [
-    'Designed and Developed a React Native module using Kotlin to handle BLE communication between Android and Apple devices for an open-source mobile app foridentity management, MOSIP',
-    'Focused on the Android BLE layer and achieved a signifi cant improvementin transfer speed,reducing transfertime from 40 seconds to approximately 1 second for150KB of data',
-    'Contributed to the development of a mobile app that enables identity managementfor millions of people across different countries and organizations',
-    'Trained and mentored junior developers and engineers, teaching skills in Clean Code and React Native to improve overallteam performance.'
+    'Developed a web application to collect employee feedback and reviews to calibrate ratings and hikes',
+    'Rewrote Node.js backend using Ruby on Rails and migrated from MongoDB to PostgresDB for a cleaner and more stable product',
+    'Worked on existing Express JS backend to add new features in parallel to new ROR backend',
+    'Contributed to all aspects of the product including creating user stories, building React UI, developing backend features, and running applications in Kubernetes',
+    'Traveled to Indonesia to discuss new requirements and provided user support for queries',
+    'Improved the Testing coverage from 40% to 90%+ for existing and new backend and frontend repositories'
   ],
-  link: new URL('https://mosip.io/'),
+  link: new URL('https://mosip.io/')
 };
 
 const experience: Experience = {
   // prettier-ignore
   heading: 'Where I\'ve Worked',
-  employments: [Mosip, GS, Gojek],
+  employments: [Mosip, GS2, GS1, Gojek]
 };
 
 const links: Links = {
   [ExternalName.Git]: new URL('https://github.com/tilak-puli/My-Profile'),
   [ExternalName.LinkedIn]: new URL('https://www.linkedin.com/in/tilak-puli-a384111a0/'),
-  [ExternalName.Instagram]: new URL('https://www.instagram.com/tilakpuli/?hl=en'),
+  [ExternalName.Instagram]: new URL('https://www.instagram.com/tilakpuli/?hl=en')
 };
 
 const myProfile: Project = {
@@ -76,7 +89,7 @@ const myProfile: Project = {
   imagePath: './assets/MyProfile.png',
   git: new URL('https://github.com/tilak-puli/My-Profile'),
   link: new URL(window.location.href),
-  techStack: ['Angular', 'nodejs'],
+  techStack: ['Angular', 'nodejs']
 };
 
 const GullyScoreboard: Project = {
@@ -86,7 +99,7 @@ const GullyScoreboard: Project = {
   imagePath: './assets/GullyScoreboard.png',
   git: new URL('https://github.com/tilak-puli/scoreboard'),
   link: new URL('https://play.google.com/store/apps/details?id=com.colossal_dreams.scoreboard'),
-  techStack: ['React Native', 'nodejs'],
+  techStack: ['React Native', 'nodejs']
 };
 
 const CashFlow: Project = {
@@ -98,17 +111,17 @@ const CashFlow: Project = {
   imagePath: './assets/Cashflow.png',
   git: new URL('https://github.com/STEP-tw/cashflow-punters'),
   link: new URL('https://cashflow-punters.herokuapp.com/'),
-  techStack: ['Javascript', 'nodejs', 'HTML', 'CSS'],
+  techStack: ['Javascript', 'nodejs', 'HTML', 'CSS']
 };
 
 export const profile: Profile = {
   name: 'Tilak Puli',
   quickIntroduction: 'I create web/mobile experiences.',
   subIntroduction:
-    "I'm a software engineer specializing in building user friendly and highly efficient web and mobile applications.",
+    'I\'m a software engineer specializing in building user friendly and highly efficient web and mobile applications.',
   about,
   experience,
   contact: { email: 'tilakpuli15@gmail.com' },
   links,
-  projects: [myProfile, GullyScoreboard, CashFlow],
+  projects: [myProfile, GullyScoreboard, CashFlow]
 };
